36d28fcbaf64b91447632cda8297645f49085343
Jade Lau <jadeyslau@gmail.com>
2023-02-12 17:18:54 +0000

    updated conf gen

--- generate-conf.json.sh
+++ generate-conf.json.sh
@@ -4,22 +4,26 @@
 
 # If variable not set or null, set to some exemplary defaults.
 theme="${1:-light}"
-mobile_phone_country_code="${2:-45}"  
-mobile_phone_number="${3:-80100100}"
-email_address="${4:-info@${theme}.dk}" 
+email_address="${4:-info@${theme}.dk}"
 
 echo $theme
 
 conf_file_path="./templates/$theme/conf.json"
 touch $conf_file_path
-echo {                                                              > $conf_file_path
-echo    "\"id"\": "\"$theme"\",                                     >> $conf_file_path
-echo    "\"signature"\": "\"Best regards,"\",                       >> $conf_file_path
-echo    "\"name"\": "\"The $theme mail team"\",                     >> $conf_file_path
-echo    "\"contactMain"\": "\"Call <a href='tel:00$mobile_phone_country_code$mobile_phone_number'><span>($mobile_phone_country_code) $mobile_phone_number</span></a> or email us at"\", >> $conf_file_path
-echo    "\"contactMail"\": "\"$email_address"\",                    >> $conf_file_path
-echo    "\"slogan"\": "\"LED Pylon. LED Wall. Digital Signage."\",  >> $conf_file_path
-echo    "\"logoUrl"\": "\"/assets/$theme.png"\",                    >> $conf_file_path
-echo    "\"logoAlt"\": "\"$theme logo"\",                           >> $conf_file_path
-echo    "\"website"\": "\"http://$theme.dk"\"                       >> $conf_file_path
-echo }                                                              >> $conf_file_path
+echo {                                                                              > $conf_file_path
+echo    "\"id"\": "\"$theme"\",                                                     >> $conf_file_path
+echo    "\"signature"\": "\"Best wishes,"\",                                        >> $conf_file_path
+echo    "\"name"\": "\"$name"\",                                                    >> $conf_file_path
+echo    "\"job-title"\": "$jobtitle"\",                                             >> $conf_file_path
+echo    "\"email"\": "\"$email_address"\",                                          >> $conf_file_path
+echo    "\"emailIcon"\": "\"/assets/email-icon-2x.png"\",                           >> $conf_file_path
+echo    "\"logoImg"\": "\"/assets/logo_colour_full_lightbg_lilac.max-800x600.png"\",>> $conf_file_path
+echo    "\"logoAlt"\": "\"L2D Logo"\",                                              >> $conf_file_path
+echo    "\"website"\": "\"https://learntodiscover.ai"\"                             >> $conf_file_path
+echo    "\"websiteIcon"\": "\"/assets/link-icon-2x.png"\"                           >> $conf_file_path
+echo    "\"bgImage"\": "\"/assets/SignatureShort.max-800x600.png"\"                 >> $conf_file_path
+echo    "\"linkedInUrl"\": "\"https://www.linkedin.com/company/learntodiscover/"\"  >> $conf_file_path
+echo    "\"linkedInIcon"\": "\"/assets/LinkedIn_icon_circle.png"\"                  >> $conf_file_path
+echo    "\"twitterUrl"\": "\"https://twitter.com/L2D_Team/"\"                       >> $conf_file_path
+echo    "\"twitterIcon"\": "\"/assets/twitter-icon-2x.png"\"                        >> $conf_file_path
+echo }                                                                              >> $conf_file_path
